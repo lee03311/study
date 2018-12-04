32795a65e928e96c69ab784928c5dcad23e04c33
slee <lee03311@naver.com>
2018-12-04 18:53:47 +0900

    181204

---
+++ server_side_javascript/excercise/cookie/app.cookie.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var app = express();
+
+app.use(cookieParser());
+app.get('/count', function(req, res){
+    var count = req.cookies.count;
+
+    if(count){
+        count++;
+    }else{
+        count = 0;
+    }
+    res.cookie('count', count);
+    res.send('count : ' + count);
+})
+
+app.listen(3003, function(){
+    console.log('Connected 3003 port....');
+});
